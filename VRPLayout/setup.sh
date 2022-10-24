e548e2ce7b9253b89e963db099c630d4434e5ca7
marowu <marowu@posteo.net>
2022-10-24 20:27:19 +0200

    add .gitignore, update setup.sh

--- VRPLayout/setup.sh
+++ VRPLayout/setup.sh
@@ -1,16 +1,18 @@
 #!/usr/bin/env bash
 
 # Create virutal environment
-python3 -m venv .venv
+python3 -m venv .venv-vrp
 
 # Activate Virtual environment
-source ./.venv/bin/activate
+source ./.venv-vrp/bin/activate
 
-# Change directory to coarsening package
-cd ./src/
+# Upgrade pip and setuptools
+pip3 install --upgrade pip setuptools
 
-# Install coarsening package
-pip3 install -e .
+# Install packages
+pip3 install -e ./src/VRPCO/
+pip3 install -e ./src/VRPGraph/
+pip3 install -e ./src/VRPQO/
 
 # Run tests
 python3 -m pytest ./test/
